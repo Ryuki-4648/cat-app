9079d32e144adc3351b6b67932e86c77bbf9246f
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-09-17 22:41:43 +0900

    config修正、eslintrc作成、マークアップ

--- cat/src/App.tsx
+++ cat/src/App.tsx
@@ -6,19 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="w-12" alt="logo" />
       </header>
+      <div className="flex justify-center flex-wrap">
+        <h1 className="w-full text-center text-3xl mb-4">ねこ専用アプリ</h1>
+        <p className="w-full text-center mb-8">ねこちゃんが好きそうな動きを選んでください。</p>
+
+        <button className="w-40">aaa</button>
+        <button>aaa</button>
+        <button>aaa</button>
+      </div>
     </div>
   );
 }
--- cat/tailwind.config.js
+++ cat/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
+  purge: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   plugins: [],
 }
 
